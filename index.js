f60aa7236e8317651e5e6a1115fa88e27102cb82
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-04-16 23:59:38 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//  top slider //
 
 document.addEventListener('DOMContentLoaded', function() {
     const ellipses = document.querySelectorAll('.slider_ellipse__container span');
@@ -23,6 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+//
+
 
 
 // slider popular categories //
@@ -31,6 +34,9 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function () {
     $('.popular_categories__card__container').slick();
 });
+
+//
+
 
 
 // slider brands //
@@ -42,3 +48,89 @@ $(".multiple-items").slick({
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+//
+
+
+
+// header menu //
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    // Отримуємо всі елементи меню
+    const menuItems = document.querySelectorAll('.nav_2__container ul li span');
+    
+    // Додаємо обробник події кліку до кожного спану
+    menuItems.forEach(function(span) {
+        // Додаємо обробник події кліку
+        span.addEventListener('click', function() {
+            // Забираємо стилі з усіх спанів
+            menuItems.forEach(function(item) {
+                item.style.color = '';
+                const img = item.parentNode.querySelector('img');
+                // Перевіряємо, чи є зображення і чи це не елемент з класом "promotional"
+                if (img && !item.classList.contains('promotional')) {
+                    img.src = './img/icons/keyboard-arrow-down.png';
+                }
+            });
+            
+            // Встановлюємо стилі для поточного спану
+            span.style.color = 'rgb(255, 125, 1)';
+            const img = span.parentNode.querySelector('img');
+            // Перевіряємо, чи є зображення і чи це не елемент з класом "promotional"
+            if (img && !span.classList.contains('promotional')) {
+                img.src = './img/icons/keyboard-arrow-top.png';
+            }
+        });
+    });
+});
+
+*/
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Отримуємо всі елементи меню
+    const menuItems = document.querySelectorAll('.nav_2__container ul li span');
+    const modalMenuContainer = document.querySelector('.modal_menu__container');
+        
+    
+    // Додаємо обробник події кліку до кожного спану
+    menuItems.forEach(function(span) {
+        // Додаємо обробник події кліку
+        span.addEventListener('click', function() {
+            // Перевіряємо, чи був вже клікнутий цей елемент
+            if (span.classList.contains('active')) {
+                // Повертаємо стиль та зображення до початкового стану
+                span.style.color = '';
+                const img = span.parentNode.querySelector('img');
+                if (img && !span.classList.contains('promotional')) {
+                    img.src = './img/icons/keyboard-arrow-down.png';
+                }
+                // Знімаємо клас "active"
+                span.classList.remove('active');
+                modalMenuContainer.style.display = 'none';
+            } else {
+                // Забираємо стилі з усіх спанів
+                menuItems.forEach(function(item) {
+                    item.style.color = '';
+                    const img = item.parentNode.querySelector('img');
+                    if (img && !item.classList.contains('promotional')) {
+                        img.src = './img/icons/keyboard-arrow-down.png';
+                    }
+                    // Знімаємо клас "active"
+                    item.classList.remove('active');
+                });
+                // Встановлюємо стилі для поточного спану
+                span.style.color = 'rgb(255, 125, 1)';
+                const img = span.parentNode.querySelector('img');
+                if (img && !span.classList.contains('promotional')) {
+                    img.src = './img/icons/keyboard-arrow-top.png';
+                }
+                // Додаємо клас "active"
+                span.classList.add('active');
+
+                modalMenuContainer.style.display = 'flex';             
+            }
+        });
+    });
+});
